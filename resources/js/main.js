52fece8e34fa487a47e9814641f41e494c5f6ad7
Stefan Damen <31634546+FinalDayz@users.noreply.github.com>
2023-10-11 19:55:17 +0200

    Moved service-worker

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,7 @@
 
 
 // Register service worker
-navigator.serviceWorker.register('resources/js/service-worker.js')
+navigator.serviceWorker.register('service-worker.js')
   .then(registration => {
     console.log('Service-worker succesvol geregistreerd');
   })
