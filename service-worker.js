ff07fb80c4672c441b1764f3b7cfb68350377a8d
Vera Compagner <51192848+veracompagner@users.noreply.github.com>
2023-10-12 14:09:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new.workbox.strategies.NetworkFirst({
+    casheName: 'EEN_VOORBEELD_CASH_KEY'
+      })
+    );
 
 // Gebruik `workbox.routing.registerRoute()`...
 
